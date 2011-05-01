91318507110aeb28c714827ec7f3878a942b3ac6
Loren Norman <lorennorman@gmail.com>
2011-05-01 16:55:11 -0400

    Get the autoscroller working in Javascript.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,33 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 $(document).ready(function() {
-  $('#animate').click(function ()
-  {
-    $("html, body").animate({ scrollTop: $(document).height() }, 3000);
-    return false;
+  var animating         = false
+    , toggleElement     = $('#animate')
+    , containerToScroll = $('#scroller')
+    , scrollTop         = 100
+    , scrollAmount      = 200
+    , scrollTime        = 1000
+    , interval          = null;
+
+  var nudgeScroll = function() {
+    scrollTop = scrollTop + scrollAmount;
+
+    containerToScroll.animate({ scrollTop: scrollTop  }, scrollTime, 'linear');
+  };
+
+  toggleElement.click(function() {
+    animating = !animating;
+
+    if(animating) {
+      interval = setInterval(nudgeScroll, scrollTime);
+    } else {
+      clearInterval(interval);
+    }
   });
-  $(window).scroll(function(){
-          if  ($(window).scrollTop() == $(document).height() - $(window).height()){
-            posts.getRow($('#posts'));
-          }
+
+  $(window).scroll(function() {
+    if  ($(window).scrollTop() == $(document).height() - $(window).height()) {
+      posts.getRow($('#posts'));
+    }
   });
+
 });
 
