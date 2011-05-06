bc102a5e7cc26ca95d46498697a1c496a27f4a63
Stafford Brooke <stafford@inquus.com>
2011-05-05 23:50:44 -0400

    removed group by 4s

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,12 +22,5 @@ class Post
   ##############
   ### Scopes ###
   ##############
-  def self.in_groups_of group_size
-    grouped = []
-    [(count/group_size.to_f).ceil, 1].max.times do |page|
-      grouped << skip(page).limit(4)
-    end
 
-    grouped
-  end
 end
