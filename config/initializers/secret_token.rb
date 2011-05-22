082353f6c9dc6134ceb5eda549d1636287b39e3f
Stafford Brooke <staffordbrooke@gmail.com>
2011-05-22 17:29:58 -0400

    removed stuff

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ImageGrid::Application.config.secret_token = 'c0e784ef7707d9ae3fc4b03af36b28c6b73b554fb2c884969b33ebfe93c46d744bfba4ccc0595b8246949fdf7c3389b58d91610c7dc8f1e41f759900ba9b1ca4'
